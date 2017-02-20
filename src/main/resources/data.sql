b14bdf55bf1e404fe62ffb5e5a08beae8abf3693
Antonio Carlos Stumpf Souto <Antonio Carlos Stumpf Souto>
2017-02-20 04:47:20 -0300

    US2 - Aceptance Criteria: O mesmo restaurante não pode ser escolhido mais de uma vez durante a semana.

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO restaurant(name) values ('Sabor Familia');
-INSERT INTO restaurant(name) values ('Currascaria Sangue na Blusa');
-INSERT INTO restaurant(name) values ('Rodizio de Salada');
